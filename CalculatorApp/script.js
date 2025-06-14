3e34bcfbcd5d6199d525f45b2940f400be953b7d
lucky habe <luckyhabe@gmail.com>
2025-06-14 18:39:10 +0200

    Updated script.js File ading if else statement

--- CalculatorApp/script.js
+++ CalculatorApp/script.js
@@ -1,5 +1,13 @@
-function btnPressed(button) {
-    let currentValue = document.getElementById("inputBox").value;
-    document.getElementById("inputBox").value = button;
+let newLine = true;
 
+function btnPressed(button) {
+    if (newLine) {
+        document.getElementById("inputBox").value=button; 
+        newLine = false;
+    } else {
+        let currentValue = document.getElementById("inputBox").value;
+    document.getElementById("inputBox").value = currentValue + button;
+
+    }
+    
 }
